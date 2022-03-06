af826fcd94e9eec5bcd021e5d188e43e9d0f2fb4
tarun10001 <97452134+tarun10001@users.noreply.github.com>
2022-03-06 21:17:40 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let num1 = 8, num2 = 6;
-let add = require("./add");
-let subtract = require("./subtract");
-let multiply = require("./multiply");
-let divide = require("./divide");
-console.log(add(num1,num2));
-console.log(subtract(num1,num2));
-console.log(multiply(num1,num2));
-console.log(divide(num1,num2));
