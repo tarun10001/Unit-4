a7666efc44489e674134e55c675019725febc195
tarunkumawat1994@gmail.com <tarunkumawat1994@gmail.com>
2022-03-06 21:36:07 +0530

    2nd assignment

---
+++ 3March Event Loop and http/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+let data = [
+    {
+        book : "The Girl With the Dragon Tattoo",
+        author : "Stieg Larsson",
+        genre : "Fictional",
+    },
+    {
+        book : "To Kill a Mockingbird",
+        author : "Harper Lee",
+        genre : "Fantasy",
+    },
+    {
+        book : "The Lovely Bones",
+        author : "Alice Sebold",
+        genre : "Mystery",
+    }
+];
+
+app.listen(4000, () => {
+    console.log("Listening on port 5000");
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.get("/books",(req, res) => {
+    res.send(data);
+})
